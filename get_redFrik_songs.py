11b6ab6d8162e6061ea9d0e905fb093d56a068b8
blitterated <blitterated@protonmail.com>
2021-03-01 19:42:13 -0600

    Fetches json tweets for a @twitterUser
    Based on https://github.com/bear/python-twitter/blob/master/examples/get_all_user_tweets.py

--- get_redFrik_songs.py
+++ get_redFrik_songs.py
@@ -5,3 +5,39 @@ import twitter
 from twcred import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 
+def get_tweets(api=None, screen_name=None):
+    timeline = api.GetUserTimeline(screen_name=screen_name, count=1000)
+    earliest_tweet = min(timeline, key=lambda x: x.id).id
+    print("getting tweets before:", earliest_tweet)
+
+    while True:
+        tweets = api.GetUserTimeline(
+            screen_name=screen_name, max_id=earliest_tweet, count=200
+        )
+        new_earliest = min(tweets, key=lambda x: x.id).id
+
+        if not tweets or new_earliest == earliest_tweet:
+            break
+        else:
+            earliest_tweet = new_earliest
+            print("getting tweets before:", earliest_tweet)
+            timeline += tweets
+
+    return timeline
+
+
+def main():
+    api = twitter.Api(consumer_key=CONSUMER_KEY,
+                      consumer_secret=CONSUMER_SECRET,
+                      access_token_key=ACCESS_TOKEN_KEY,
+                      access_token_secret=ACCESS_TOKEN_SECRET)
+    screen_name = sys.argv[1]
+    print(screen_name)
+    timeline = get_tweets(api=api, screen_name=screen_name)
+
+    for json_tweet in timeline:
+        print(json.dumps(json_tweet._json))
+
+
+if __name__ == "__main__":
+    main()
